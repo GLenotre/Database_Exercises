63dae5107d4943a2250e5774b04c3eda46917839
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-15 11:41:26 -0500

    wrote queries to find out information from the table

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,8 +4,8 @@
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name  VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
-    sales FLOAT NOT NULL,
+    release_date YEAR NOT NULL,
     genre VARCHAR (50) NOT NULL,
+    sales FLOAT NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ select_exercise.sql
@@ -0,0 +1,3 @@
+USE codeup_test_db;
+SELECT genre FROM albums
+WHERE name = "Nevermind";
