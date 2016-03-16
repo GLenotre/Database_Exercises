e524b0abb4e0f0bfcd76084d6cc1f4e3e8f7c24f
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-16 09:58:05 -0500

    found employees born on Christmas and hired in the 90s

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,3 +3,14 @@ FROM employees
 WHERE last_name LIKE 'Z%'
 GROUP BY last_name DESC
 LIMIT 10 OFFSET 0;
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%12-25'
+AND hire_date LIKE '199%'
+LIMIT 10 OFFSET 0;
