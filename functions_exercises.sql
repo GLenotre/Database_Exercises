568a206d00a63eb55ea17bc98614b37426c2e7fd
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-16 11:04:42 -0500

    used concat() to combine first and last names together as a single column

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,3 +2,7 @@ SELECT count(gender) AS "Gender count", gender
 FROM employees
 WHERE first_name IN ('irena', 'maya', 'vidya')
 GROUP BY gender;
+
+SELECT concat(first_name, ' ', last_name) AS "Full Name"
+FROM employees
+WHERE last_name LIKE 'e%e';
