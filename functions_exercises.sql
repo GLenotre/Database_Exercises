30e5d600f968462d69f59fc91ea092765f3aeb67
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-16 11:12:50 -0500

    used datediff() to find how many days people  have been working at the company

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -6,3 +6,10 @@ GROUP BY gender;
 SELECT concat(first_name, ' ', last_name) AS "Full Name"
 FROM employees
 WHERE last_name LIKE 'e%e';
+
+
+SELECT concat(first_name, ' ', last_name) AS "Full Name", count(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY full_name;
